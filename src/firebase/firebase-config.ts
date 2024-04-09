7791b4de90cef0c759787252d31e00ce64a9ba68
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-04-10 07:08:10 +0900

    config babelrc and customize-cra to write import path faster

---
+++ src/firebase/firebase-config.ts
@@ -0,0 +1,12 @@
+export default function firebaseConfig() {
+	return {
+		apiKey: "1",
+		authDomain: "1",
+		databaseURL: "1",
+		projectId: "1",
+		storageBucket: "1",
+		messagingSenderId: "1",
+		appId: "1",
+		measurementId: "1",
+	};
+}
