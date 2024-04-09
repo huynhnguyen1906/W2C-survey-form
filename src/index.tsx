c5b94db9a5996cd875dd483c2b284dce9f95b16d
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-04-10 08:04:33 +0900

    Add normalize.css and sass dependencies... And setting default css

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import GlobalStyles from "./components";
 import App from "~/App";
 import reportWebVitals from "./reportWebVitals";
+import "normalize.css";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<GlobalStyles>
+			<App />
+		</GlobalStyles>
 	</React.StrictMode>
 );
 
