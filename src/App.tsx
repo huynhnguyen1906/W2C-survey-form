35f3418efe3bf30f23399b3c2d4de01950684c35
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2024-04-10 13:41:18 +0900

    Add react-router-dom dependency and update App.tsx to use routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Test from "./pages/Test";
+import Home from "./pages/Home/Home";
+import SurveyList from "./pages/SurveyList/SurveyList";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>hello</h1>
-			<Test />
-		</div>
+		<Router>
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="survey-list" element={<SurveyList />} />
+					<Route path="test" element={<Test />} />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
