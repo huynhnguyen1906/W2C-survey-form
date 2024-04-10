e7bf70f097837e7864eed5952ea87b362122e45e
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-04-11 00:25:17 +0900

    Add CompleteDisplay component and update Home.tsx to display it after form submission

---
+++ src/components/CompleteDisplay/CompleteDisplay.tsx
@@ -0,0 +1,10 @@
+function CompleteDisplay() {
+	return (
+		<div className="completeDisplay">
+			<h3>申請完了！</h3>
+			<h3>連絡をお持ちください！</h3>
+		</div>
+	);
+}
+
+export default CompleteDisplay;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,14 @@ import db from "~/firebase/firebase-config";
 import { collection, addDoc } from "firebase/firestore";
 import { useState, useRef } from "react";
 import Header from "~/components/Header/Header";
+import CompleteDisplay from "~/components/CompleteDisplay/CompleteDisplay";
 
 function Home() {
 	const [studentId, setStudentId] = useState("");
 	const [classId, setClassId] = useState("");
 	const [name, setName] = useState("");
 	const [message, setMessage] = useState("");
+	const [isSubmitted, setIsSubmitted] = useState(false);
 
 	const studentIdInputRef = useRef<HTMLInputElement>(null);
 
@@ -36,6 +38,7 @@ function Home() {
 				created_at: new Date(),
 				have_read: false,
 			});
+			setIsSubmitted(true);
 		} catch (e) {
 			console.error("Error adding document: ", e);
 		}
@@ -54,63 +57,67 @@ function Home() {
 				<img src="/logo.svg" alt="logo" />
 			</div>
 			<div className="main">
-				<div className="inputForm">
-					<div className="inputBox">
-						<table>
-							<tbody>
-								<tr>
-									<td>学籍番号</td>
-									<td>
-										<span>*入力必須</span>
-										<input
-											type="text"
-											ref={studentIdInputRef}
-											value={studentId}
-											onChange={(e) => setStudentId(e.target.value)}
-										/>
-									</td>
-								</tr>
-								<tr>
-									<td>クラス</td>
-									<td>
-										<span>*入力必須</span>
-										<input
-											type="text"
-											value={classId}
-											onChange={(e) => setClassId(e.target.value)}
-										/>
-									</td>
-								</tr>
-								<tr>
-									<td>名前</td>
-									<td>
-										<span>*入力必須</span>
-										<input
-											type="text"
-											value={name}
-											onChange={(e) => setName(e.target.value)}
-										/>
-									</td>
-								</tr>
-								<tr>
-									<td>一言</td>
-									<td>
-										<textarea
-											placeholder="質問、相談、やりたいこと、なんでも。。。。。"
-											value={message}
-											onChange={(e) => setMessage(e.target.value)}
-										></textarea>
-									</td>
-								</tr>
-							</tbody>
-						</table>
+				{isSubmitted ? (
+					<CompleteDisplay />
+				) : (
+					<div className="inputForm">
+						<div className="inputBox">
+							<table>
+								<tbody>
+									<tr>
+										<td>学籍番号</td>
+										<td>
+											<span>*入力必須</span>
+											<input
+												type="text"
+												ref={studentIdInputRef}
+												value={studentId}
+												onChange={(e) => setStudentId(e.target.value)}
+											/>
+										</td>
+									</tr>
+									<tr>
+										<td>クラス</td>
+										<td>
+											<span>*入力必須</span>
+											<input
+												type="text"
+												value={classId}
+												onChange={(e) => setClassId(e.target.value)}
+											/>
+										</td>
+									</tr>
+									<tr>
+										<td>名前</td>
+										<td>
+											<span>*入力必須</span>
+											<input
+												type="text"
+												value={name}
+												onChange={(e) => setName(e.target.value)}
+											/>
+										</td>
+									</tr>
+									<tr>
+										<td>一言</td>
+										<td>
+											<textarea
+												placeholder="質問、相談、やりたいこと、なんでも。。。。。"
+												value={message}
+												onChange={(e) => setMessage(e.target.value)}
+											></textarea>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<div className="submitBtn">
+							<button onClick={handleSubmit} className="btn">
+								送信
+							</button>
+						</div>
 					</div>
-					<div className="submitBtn">
-						<button onClick={handleSubmit} className="btn">
-							送信
-						</button>
-					</div>
-				</div>
+				)}
 			</div>
 		</div>
 	);
