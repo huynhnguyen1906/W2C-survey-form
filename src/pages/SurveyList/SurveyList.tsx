771b61649d56eff1a67dc5f1a8a9b18879d1a23e
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2024-04-10 13:43:27 +0900

    Add SurveyList component

---
+++ src/pages/SurveyList/SurveyList.tsx
@@ -0,0 +1,12 @@
+import Header from "~/components/Header/Header";
+
+function SurveyList() {
+	return (
+		<div>
+			<Header />
+			<h1>SurveyList</h1>
+		</div>
+	);
+}
+
+export default SurveyList;
